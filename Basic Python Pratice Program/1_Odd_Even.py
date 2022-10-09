5b79381493612bb8cfe0465bd013ec1e04402ff7
Saurabh Barasiya <43789500+Saurabh1Barasiya@users.noreply.github.com>
2022-10-09 14:02:07 +0530

    Update 1_Odd_Even.py
    Number is even or Odd

---
+++ Basic Python Pratice Program/1_Odd_Even.py
@@ -0,0 +1,7 @@
+# this program give you number is even or odd.👀👀👀
+
+number = int(input("Enter number to check its even or odd : "))
+if number % 2 == 0:
+    print(f"Number {number} is even ✔✔✔")
+else:
+    print(f"Number {number} is odd ")
